c2958e10d9d53ef869466196bcaa6aea6c75a863
FedMG <federico.gonzalia@gmail.com>
2023-01-05 08:47:49 +0000

    Update 2 files

--- src/additional.d.ts
+++ src/additional.d.ts
@@ -6,3 +6,14 @@ export interface RootLayout {
   children: React.ReactNode
   title: string
 }
+
+export interface LinkColumns {
+  id: number
+  title: string
+  column: LinkProps[]
+}
+
+export interface LinkProps {
+  path: string
+  route: string
+}
