3b4cde8e38f00b5deb2f0a0ce41ee43e1804da53
FedMG <federico.gonzalia@gmail.com>
2023-01-14 02:25:23 +0000

    Refactor(_app): types and move them to _app-types

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,12 @@
-import { ReactElement, ReactNode } from 'react'
-import type { AppProps } from 'next/app'
-import { NextPage } from 'next'
+import { ReactNode } from 'react'
+import { AppPropsWithLayout } from '_app-types'
 
 import 'styles/globals.css'
 
-export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
-  getLayout?: (page: ReactElement) => ReactNode
-}
-
-type AppPropsWithLayout = AppProps & {
-  Component: NextPageWithLayout
-}
-
-export default function App ({ Component, pageProps }: AppPropsWithLayout): ReactNode {
+export default function App ({
+  Component,
+  pageProps
+}: AppPropsWithLayout): ReactNode {
   const getLayout = Component.getLayout ?? ((page) => page)
-  return getLayout(<Component {...pageProps} />)
+  return getLayout(<Component {...pageProps} />, pageProps)
 }
